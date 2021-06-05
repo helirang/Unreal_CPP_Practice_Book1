eb8577dda27af684474445a1b53b126231609144
광현 최 <helirang34@gmail.com>
2021-06-06 01:50:53 +0900

    ch13 end

--- ArenaBattle/Private/ABSection.cpp
+++ ArenaBattle/Private/ABSection.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ABSection.h"
+#include "ABCharacter.h"
+#include "ABItemBox.h"
 
 
 // Sets default values
@@ -60,6 +62,9 @@ AABSection::AABSection()
 	}
 
 	bNoBattle = false;
+
+	EnemySpawnTime = 2.0f;
+	ItemBoxSpawnTime = 5.0f;
 }
 
 // Called when the game starts or when spawned
@@ -93,6 +98,17 @@ void AABSection::SetState(ESetctionState NewState)
 			GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
 		}
 		OperateGates(false);
+
+		GetWorld()->GetTimerManager().
+			SetTimer(SpawnNPCTimeHandle, FTimerDelegate::CreateUObject(this, &AABSection::OnNPCSpawn), EnemySpawnTime, false);
+
+		GetWorld()->GetTimerManager().SetTimer(SpawnBoxTimeHandle, FTimerDelegate::CreateLambda([this]()->void {
+		
+			FVector2D RandXY = FMath::RandPointInCircle(600.0f);
+			GetWorld()->SpawnActor<AABItemBox>(GetActorLocation() + FVector(RandXY, 30.0f), FRotator::ZeroRotator);
+		
+		}), ItemBoxSpawnTime, false);
+
 		break;
 	}
 	case AABSection::ESetctionState::COMPLETE:
@@ -164,4 +180,9 @@ void AABSection::OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedCompon
 	{
 		ABLOG(Warning, TEXT("New section area is not empty."));
 	}
+}
+
+void AABSection::OnNPCSpawn()
+{
+	GetWorld()->SpawnActor<AABCharacter>(GetActorLocation() + FVector::UpVector * 88.0f, FRotator::ZeroRotator);
 }
--- ArenaBattle/Public/ABSection.h
+++ ArenaBattle/Public/ABSection.h
@@ -41,6 +41,8 @@ private:
 	void OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 		bool bFromSweep, const FHitResult &SweepResult);
 
+	void OnNPCSpawn();
+
 private:
 	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
 	TArray<UStaticMeshComponent*> GateMeshes;
@@ -56,4 +58,13 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = State, Meta=(AllowPrivateAccess = true))
 	bool bNoBattle;
+
+	UPROPERTY(EditAnywhere, Category = spawn, Meta = (AllowPrivateAccess = true))
+	float EnemySpawnTime;
+
+	UPROPERTY(EditAnywhere, Category = spawn, Meta=(AllowPrivateAccess = true))
+	float ItemBoxSpawnTime;
+
+	FTimerHandle SpawnNPCTimeHandle = {};
+	FTimerHandle SpawnBoxTimeHandle = {};
 };
